6fb4baebba449f55e4feebbedacbf645ba75e022
Rada02-coder <matteoradaelli02@gmail.com>
2026-03-17 20:35:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import plotly.express as px
 
 # --- CONFIGURAZIONE ---
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzsd7D1e52RU36XtZ1e9nCWTpuIXZgvjah-RjswamliR-zeWmmD4oNU1O2qgOLDUc1y/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzXHnZx0YJOEY4ar0UFKxuSe53XXGtTuyZTEF7tNF-WDqm6mYMRolw5TnHcT4D4ikGw/exec"
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1aYMaJ9ZmkD4lo0-dgr3TZKhGr9_kTgKpAOq67nt7xs4/edit?gid=0#gid=0"
 
 st.set_page_config(page_title="Pocket Manager", page_icon="💳")
